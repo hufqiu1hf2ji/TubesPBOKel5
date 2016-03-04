c942b19db6534d462ccbff4d737578df71104a19
wendianto <wendianto.96@gmail.com>
2016-03-04 16:11:14 +0700

    Wendi

---
+++ SisFoManagementProyek/src/sisfomanagementproyek/Proyek.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sisfomanagementproyek;
+
+/**
+ *
+ * @author Notonogoro
+ */
+/*public class Proyek{
+	private Programmer programmer[];
+	private Tugas tugas[];
+	private String namaProyek;
+	private long idProyek;
+	private String deadline;
+	private long anggaranProyek;
+	private boolean statusProyek;
+	private String tentangProyek;
+
+	public Proyek(String namaProyek, String deadline, long anggaranProyek, String tentangProyek){
+
+	}
+
+	public long createID(){
+
+	}
+	
+	public void ctugasEngine(String namaTugas, String deadline){
+
+	}
+
+	public void aprogrammerEngine(Programmer namaProgrammer){
+
+	}
+
+	public void mprogrammerEngine(Programmer namaProgrammer){
+
+	}
+
+	public void spelaksanaEngine(String namaTugas, Programmer namaProgrammer){
+
+	}
+
+	public int searchProgrammer(String programmer){
+
+	}
+
+	public int searchTugas(String tugas){
+
+	}
+
+}*/
