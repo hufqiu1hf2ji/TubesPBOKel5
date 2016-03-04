cc99ff924cbf1ef45b2f9c0845a3a9f09dc50d7b
wendianto <wendianto.96@gmail.com>
2016-03-04 16:13:22 +0700

    Ulya

---
+++ SisFoManagementProyek/src/sisfomanagementproyek/Tugas.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sisfomanagementproyek;
+
+/**
+ *
+ * @author Notonogoro
+ */
+/*public class Tugas{
+	private Programmer pelaksana;
+	private String namaTugas;
+	private long idTugas;
+	private String deadline;
+	private String statusTugas;
+
+	public Tugas(String namaTugas, String deadline){
+
+	}
+
+	public generateStatus(String namaTugas){
+		statusTugas = pelaksana.getDataTugas(namaTugas);		
+	}
+
+	public long createID(){
+
+	}
+
+	public void setPelaksana(Programmer p){
+
+	}
+	
+}*/
