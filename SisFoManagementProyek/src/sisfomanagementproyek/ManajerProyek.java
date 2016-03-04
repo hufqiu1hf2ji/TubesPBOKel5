fc66f596f931299b81dcf1126018a3a51283e259
wendianto <wendianto.96@gmail.com>
2016-03-04 14:06:50 +0700

    Lintang

---
+++ SisFoManagementProyek/src/sisfomanagementproyek/ManajerProyek.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sisfomanagementproyek;
+
+/**
+ *
+ * @author Notonogoro
+ */
+    /*public class ManajerProyek extends Orang{
+	private Proyek proyek[];
+	private int numProyek;
+	private long budget;
+
+	public ManajerProyek(String firstNama, String lastNama, String jenisKelamin, String tanggalLahir, String telepon, String alamat,String kota,long budget){
+		super(firstNama,lastNama,jenisKelamin,tanggalLahir,telepon,alamat,kota);
+		this.budget = budget;
+		id = createID();
+	}
+
+	public long createID(){
+
+	}
+
+	public void createProyek(String namaProyek,String deadline, long anggaranProyek, String tentangProyek){
+
+	}
+
+	public void createTugas(String namaProyek, String namaTugas,String deadline){
+
+	}
+
+	public void addProgrammer(String namaProyek, String namaProgrammer){
+
+	}
+
+	public void minProgrammer(String namaProyek, String namaProgrammer){
+
+	}
+
+	public void setPelaksanaTugas(String namaProyek, String namaTugas, String namaProgrammer){
+
+	}
+
+	public int searchProyek(String namaProyek){
+            
+        }
+
+}*/
