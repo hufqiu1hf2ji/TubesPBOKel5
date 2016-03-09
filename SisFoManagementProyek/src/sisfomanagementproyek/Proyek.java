a58257bd7d6ac4cba910ce17020de6633fd26244
Wahyu Lintang S <tasaucup@gmail.com>
2016-03-09 09:49:23 +0700

    proyek
    www

--- SisFoManagementProyek/src/sisfomanagementproyek/Proyek.java
+++ SisFoManagementProyek/src/sisfomanagementproyek/Proyek.java
@@ -1,65 +1,92 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package sisfomanagementproyek;
-public class Proyek{
-	private Programmer[10]programmer;
-	private String nama;
-	private int numProgrammer;
-	private Tugas[10]tugas;
-	private int numTugas;
-	private date deadline;
 
-	public Proyek(String namaProyek, date deadline){
-		this.namaProyek=namaProyek;
-		this.deadline=deadline;
+import java.util.Date;
+
+/**
+ *
+ * @author Notonogoro
+ */
+    public class Proyek{
+	private String namaProyek;
+        private Programmer programmer[]=new Programmer[10];
+	private Tugas tugas[]=new Tugas[10];
+	private int numProgrammer;
+        private int numTugas;
+        private Date deadline;
+        
+	public Proyek(String namaProyek, Date deadline){
+        this.namaProyek=namaProyek;
+		this.deadline=deadline;   
 	}
 	
-	public void addProgrammer(Programmer p){
-		if (numProgrammer<10){
+        public void addProgrammer(Programmer p){
+            //add Programmer ketika numProgrammer kurang dari maxProgrammer
+			if (numProgrammer<10){
 			programmer[numProgrammer]=p;
 			numProgrammer++;
 		}
-	}
-	
-	public void createTugas(String namaTugas){
-		if (numTugas<10){
+		}
+        
+        public void createTugas(String namaTugas){
+            //create Tugas ketika numTugas kurang dari max Tugas
+			if (numTugas<10){
 			tugas[numTugas]=p;
 			numTugas++;
 		}
-	}
-	
-	public void removeProgrammer(int indexProgrammer){
-		for (i=indexProgrammer;i<numProgrammer;i++){
+        }
+        
+        public void removeProgrammer(int indexProgrammer){
+            /*melakukan remove pada array dan merapihkan posisi array
+                remove dilakukan dengan algoritma
+                Jika verifikasiStatus Programmer == true || Tugas Programmer == null,bila tidak berarti masih ada tugas yang belum selesai.
+            */
+			for (i=indexProgrammer;i<numProgrammer;i++){
 			programmer[i]=programmer[i+1]
 		}
 		programmer[numProgrammer]=null;
 		numProgrammer--;
-	}
-	
-	public void removeTugas(int indexTugas){
-		for (i=indexTugas;i<numTugas;i++){
+        }
+        
+        public void removeTugas(int indexTugas){
+            /*melakukan remove pada array dan merapihkan posisi array
+                remove dilakukan dengan algoritma 
+                Jika Tugas[indexTugas].getStatus() == 1 , maka remove tugas dilakukan.
+                Jika tidak, Tugas tidak dapat dihapus.
+            */
+			for (i=indexTugas;i<numTugas;i++){
 			tugas[i]=tugas[i+1]
 		}
 		tugas[numTugas]=null;
 		numTugas--;
-	}
-	
-	public Tugas getTugas(int indexTugas){
-			return Tugas[indexTugas];
-	}
-	
-	public Programmer getProgrammer[int indexProgrammer]{
-			return Programmer[indexProgrammer];
-	}
-	
-	public void setNama(String nama){
-		this.nama=nama;
-	}
-	
-	public String getNama(){
+        }
+        
+        public Tugas getTugas(int indexTugas){
+			return Tugas[indexTugas];     
+        }
+        
+        public Programmer getProgrammer(int indexProgrammer){
+            return Programmer[indexProgrammer];
+        }
+        
+        public String getNama(){
+            //return namaProyek
+			this.nama=nama;
+        }
+        
+        public void setNama(){
+            //set namaProyek
 			return nama;
-	}
-	
-	public date getDeadline(){
+        }
+        
+        public Date getDeadline(){
+            //return deadline
 			return deadline;
-	}
-}
+        }
+        
+        
+    }
