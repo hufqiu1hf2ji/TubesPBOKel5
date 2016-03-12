de4d7f48045089ec9c244a0f9b27c508bf7df359
wendianto <wendianto.96@gmail.com>
2016-03-12 15:27:02 +0700

    Lintang

--- SisFoManagementProyek/src/sisfomanagementproyek/Proyek.java
+++ SisFoManagementProyek/src/sisfomanagementproyek/Proyek.java
@@ -20,37 +20,46 @@ import java.util.Date;
         private Date deadline;
         
 	public Proyek(String namaProyek, Date deadline){
-        this.namaProyek=namaProyek;
-		this.deadline=deadline;   
+            this.namaProyek=namaProyek;
+            this.deadline=deadline;   
 	}
 	
         public void addProgrammer(Programmer p){
             //add Programmer ketika numProgrammer kurang dari maxProgrammer
-			if (numProgrammer<10){
-			programmer[numProgrammer]=p;
-			numProgrammer++;
-		}
-		}
+            if (numProgrammer<10){
+                programmer[numProgrammer]=p;
+		numProgrammer++;
+            } else System.out.println("Programmer pada Proyek "+namaProyek+" sudah melebihi batas");
+	}
         
         public void createTugas(String namaTugas){
             //create Tugas ketika numTugas kurang dari max Tugas
-			if (numTugas<10){
-			tugas[numTugas]=p;
-			numTugas++;
-		}
+            if (numTugas<10){
+                    tugas[numTugas]= new Tugas(namaTugas);
+		    numTugas++;
+	    } else System.out.println("Tugas pada Proyek "+namaProyek+" sudah melebihi batas");
         }
         
         public void removeProgrammer(int indexProgrammer){
             /*melakukan remove pada array dan merapihkan posisi array
                 remove dilakukan dengan algoritma
-                Jika verifikasiStatus Programmer == true || Tugas Programmer == null,bila tidak berarti masih ada tugas yang belum selesai.
+                Jika verifikasiStatus Programmer == true || Tugas Programmer == null,bila tidak berarti masih ada tugas yang belum selesai/terverifikasi.
             */
-			for (i=indexProgrammer;i<numProgrammer;i++){
-			programmer[i]=programmer[i+1]
-		}
-		programmer[numProgrammer]=null;
-		numProgrammer--;
-        }
+            if (indexProgrammer<=numProgrammer){
+                for(int i=0;i<numTugas;i++){
+                    tugas[i].getStatus();
+                }
+                if(programmer[indexProgrammer].verifikasiStatus == true || programmer[indexProgrammer].tugas == null){
+                    if(indexProgrammer != numProgrammer){
+                        for (int i=indexProgrammer;i<numProgrammer;i++){
+                            programmer[i]=programmer[i+1];
+                        }
+                    }
+                    programmer[numProgrammer]=null;
+                    numProgrammer--;
+                }else System.out.println("Hapus Programmer gagal. Programmer belum menyelesaikan tugas "+programmer[indexProgrammer].tugas);
+            }else System.out.println("Mohon input indexProgrammer tidak melebihi "+numProgrammer);
+	}
         
         public void removeTugas(int indexTugas){
             /*melakukan remove pada array dan merapihkan posisi array
@@ -58,34 +67,44 @@ import java.util.Date;
                 Jika Tugas[indexTugas].getStatus() == 1 , maka remove tugas dilakukan.
                 Jika tidak, Tugas tidak dapat dihapus.
             */
-			for (i=indexTugas;i<numTugas;i++){
-			tugas[i]=tugas[i+1]
-		}
-		tugas[numTugas]=null;
-		numTugas--;
+            if (indexTugas<=numTugas) {
+       		if (tugas[indexTugas].getStatus() == 1){
+                    if (indexTugas != numTugas){
+                        for (int i=indexTugas;i<numTugas;i++){
+                            tugas[i]=tugas[i+1];
+                        }
+                    }
+                    tugas[numTugas]=null;
+                    numTugas--;
+                }else System.out.println("Hapus Tugas gagal.Tugas belum selesai");
+            }else System.out.println("Mohon input indexTugas tidak melebihi "+numTugas);
         }
         
         public Tugas getTugas(int indexTugas){
-			return Tugas[indexTugas];     
+            if (indexTugas<=numTugas){
+                return tugas[indexTugas];
+            }else return null;
         }
         
         public Programmer getProgrammer(int indexProgrammer){
-            return Programmer[indexProgrammer];
+            if (indexProgrammer<=numProgrammer){
+                return programmer[indexProgrammer];
+            }else return null;
         }
         
         public String getNama(){
             //return namaProyek
-			this.nama=nama;
+            return namaProyek;
         }
         
-        public void setNama(){
+        public void setNama(String namaProyek){
             //set namaProyek
-			return nama;
+            this.namaProyek=namaProyek;	
         }
         
         public Date getDeadline(){
             //return deadline
-			return deadline;
+            return deadline;
         }
         
         
