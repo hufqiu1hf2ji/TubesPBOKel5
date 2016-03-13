37cc98da341bbbfee66e5635eab355e98677687f
Wahyu Lintang S <tasaucup@gmail.com>
2016-03-14 00:26:33 +0700

    LintangMANPRO

--- SisFoManagementProyek/src/sisfomanagementproyek/ManajerProyek.java
+++ SisFoManagementProyek/src/sisfomanagementproyek/ManajerProyek.java
@@ -5,38 +5,76 @@
  */
 package sisfomanagementproyek;
 
+import java.util.Date;
+
 /**
  *
  * @author Notonogoro
  */
-    public class ManajerProyek extends Orang{
-	private Proyek proyek[] = new Proyek[10];
-	private int numProyek;
+public class ManajerProyek extends Orang {
 
-	public ManajerProyek(String nama,String jenisKelamin, String telepon, String alamat){
-            super(nama,jenisKelamin,telepon,alamat);
-            numProyek = 0;
-	}
+    private Proyek[] proyek;
+    private int numProyek = 0;
 
-	public void createProyek(String namaProyek,Date deadline){
-            //me-create obyek proyek dan menginputkannya di array proyek,lalu menambahkan numProyek, dibentuk proyek bila namaProyek belum pernah ada dan numProyek < maxProyek
-		if(numProyek<10){
-			proyek[numProyek]=p;
-			numProyek++;
-		}
-	}
-        
-        public Proyek getProyek(int index){
-            //Mereturn proyek pada array proyek dengan nomor index
-			return Proyek[index];
-		}
-        
-        public void deleteProyek(int index){
-            //Menghapus proyek pada array proyek dengan nomor index lalu menyusun array proyek
-			for (i=index;i<numProyek;i++){
-			Proyek[i]=Proyek[i+1]
-		}
-		Proyek[numProyek]=null;
-		numProyek--;
-		}
+    public ManajerProyek(String nama, String jenisKelamin, String telepon, String alamat) {
+        super(nama, jenisKelamin, telepon, alamat);
+        proyek = new Proyek[10];
+    }
+
+    public ManajerProyek(String nama, String jenisKelamin, String telepon, String alamat, int maxProyek) {
+        super(nama, jenisKelamin, telepon, alamat);
+        proyek = new Proyek[maxProyek];
+    }
+
+    public void createProyek(String namaProyek, Date deadline) {
+        if (numProyek < proyek.length) {
+            proyek[numProyek] = new Proyek(namaProyek, deadline);
+            numProyek++;
+            System.out.println("![SUKSES]-Proyek " + namaProyek + " berhasil ditambahkan.");
+        } else {
+            System.out.println("![INFO]-Gagal.Proyek sudah melebihi batas");
+        }
+    }
+
+    public void createProyek(String namaProyek, Date deadline, int maxTugas) {
+        if (numProyek < proyek.length) {
+            proyek[numProyek] = new Proyek(namaProyek, deadline, maxTugas);
+            numProyek++;
+            System.out.println("![SUKSES]-Proyek " + namaProyek + " berhasil ditambahkan.");
+        } else {
+            System.out.println("![INFO]-Gagal. Proyek sudah melebihi batas");
+        }
+    }
+
+    public Proyek getProyek(int index) {
+        if (index <= numProyek) {
+            return proyek[index];
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteProyek(int index) {
+        if ((index < numProyek) && (index >= 0)) {
+            System.out.println("![SUKSES]-Proyek "+proyek[index].getNamaProyek() + " telah terhapus");
+            proyek[index] = null;
+            //mekanisme penggeseran
+            for (int i = index; i < numProyek; i++) {
+                proyek[i] = proyek[i + 1];
+            }
+            proyek[numProyek]=null;
+            numProyek--;
+            if (numProyek == 0) {
+                System.out.println("![INFO]-List Proyek Manager Proyek habis.");
+            }
+
+        } else {
+            System.out.println("![INFO]-Gagal.Tidak ada proyek yang terdeteksi");
+        }
+    }
+
+    public int getNumProyek() {
+        return numProyek;
+    }
+
 }
