5c59f9f0277738ee062f8bdd6c648610ca0cd2d6
andiniulya <ahanifahulya@yahoo.com>
2016-03-14 10:01:56 +0700

    Update Orang.java

--- SisFoManagementProyek/src/sisfomanagementproyek/Orang.java
+++ SisFoManagementProyek/src/sisfomanagementproyek/Orang.java
@@ -10,48 +10,50 @@ package sisfomanagementproyek;
  * @author Notonogoro
  */
 public abstract class Orang {
-	private String nama;
-        private String alamat;
-        private String telepon;
-        private String jenisKelamin;
 
-	public Orang(String nama,String jenisKelamin, String telepon, String alamat){
-		this.nama = nama;
-		this.jenisKelamin = jenisKelamin;
-		this.telepon = telepon;
-		this.alamat = alamat;
-	}
+    private String nama;
+    private String alamat;
+    private String telepon;
+    private String jenisKelamin;
 
-        public void setNama(String nama){
-            this.nama=nama;
-        }
-        
-        public String getNama(){
-            return nama;
-        }
-        
-        public void setAlamat(String alamat){
-            this.alamat=alamat;
-        }
-        
-        public String getAlamat(){
-            return alamat;
-        }
-        
-        public void setTelepon(String telepon){
-            this.telepon=telepon;
-        }
-        
-        public String getTelepon(){
-            return telepon;
-        }
-        
-        public void setJenisKelamin(String jenisKelamin){
-            this.jenisKelamin=jenisKelamin;
-        }
-        
-        public String getJenisKelamin(){
-            return jenisKelamin;
-        }
+    public Orang(String nama, String jenisKelamin, String telepon, String alamat) {
+        this.nama = nama;
+        this.jenisKelamin = jenisKelamin;
+        this.telepon = telepon;
+        this.alamat = alamat;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setTelepon(String telepon) {
+        this.telepon = telepon;
+    }
+
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
 }
-//Wendi maafin kl msh ada atau byk yg salah
+
