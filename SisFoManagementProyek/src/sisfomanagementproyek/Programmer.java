c6fc6813755ef42841dafa76500531e78e11b0fc
wendianto <wendianto.96@gmail.com>
2016-03-14 10:30:37 +0700

    Wendi

--- SisFoManagementProyek/src/sisfomanagementproyek/Programmer.java
+++ SisFoManagementProyek/src/sisfomanagementproyek/Programmer.java
@@ -9,14 +9,66 @@ package sisfomanagementproyek;
  *
  * @author Notonogoro
  */
-    public class Programmer extends Orang{
-	public String tugas;
-        public int status;
-        public boolean verifikasiStatus;
-	
-        public Programmer(String nama,String jenisKelamin, String telepon, String alamat){
-            super(nama,jenisKelamin,telepon,alamat);
-            status = 0;
-            verifikasiStatus = false;
-	}
+public class Programmer extends Orang {
+
+    public Programmer(String nama, String jenisKelamin, String telepon, String alamat) {
+        super(nama, jenisKelamin, telepon, alamat);
     }
+
+    public void setStatusTugas(Tugas t) {
+        if (t != null && t.getPelaksana() == this) {
+            t.setStatus(1);
+            System.out.println("![SUKSES]-Tugas [" + t.getNamaTugas() + "] sudah diselesaikan.");
+        } else {
+            System.out.println("![GAGAL]-Tugas tidak ditemukan");
+        }
+    }
+
+    //mengecek apakah programmer termasuk didalam proyek atau tidak
+    public boolean getProgrammerDalamProyek(Proyek p) {
+        boolean a = false;
+        for (int i = 0; i < p.getNumProgrammer(); i++) {
+            if (p.getProgrammer(i).equals(this)) {
+                a = true;
+            }
+        }
+        return a;
+    }
+
+    public void listProyek(ManajerProyek mp) {
+        int x = 0;
+        System.out.println("![INFO]-List Proyek "+super.getNama()+" pada Manajer Proyek "+mp.getNama());
+        for (int i = 0; i < mp.getNumProyek(); i++) {
+            for (int j = 0; j < mp.getProyek(i).getNumProgrammer(); j++) {
+                if (mp.getProyek(i).getProgrammer(j).equals(this)) {
+                    System.out.println("        "+x + ". " + mp.getProyek(i).getNamaProyek());
+                    x++;
+                }
+            }
+        }
+    }
+
+    public void listTugas(ManajerProyek mp, Proyek p) {
+        if(getProgrammerDalamProyek(p)){
+        System.out.println("![INFO]-List Tugas "+super.getNama()+" pada Proyek "+p.getNamaProyek()+" :");
+        for (int i = 0; i < mp.getNumProyek(); i++) {
+            if (mp.getProyek(i).equals(p)) {
+                int x = 0;
+                for (int j = 0; j < mp.getProyek(i).getNumTugas(); j++) {
+                    if(mp.getProyek(i).getTugas(j).getPelaksana()!=null){
+                    if (mp.getProyek(i).getTugas(j).getPelaksana().equals(this) && mp.getProyek(i).getTugas(j).getStatus()==0) {
+                        System.out.println("        "+x + ". " + mp.getProyek(i).getTugas(j).getNamaTugas());
+                        x++;
+                    }
+                    }
+                }
+            }
+        }
+    }else System.out.println(super.getNama()+" tidak berada dalam Proyek "+p.getNamaProyek());
+    }
+
+    public Tugas getTugas(Proyek p, int indexTugas) {
+        return p.getTugas(indexTugas);
+    }
+
+}
