d2c15f12adffeaac3121e81c95c42d3cf2c2335e
andiniulya <ahanifahulya@yahoo.com>
2016-03-14 10:02:00 +0700

    Update Tugas.java

--- SisFoManagementProyek/src/sisfomanagementproyek/Tugas.java
+++ SisFoManagementProyek/src/sisfomanagementproyek/Tugas.java
@@ -9,49 +9,53 @@ package sisfomanagementproyek;
  *
  * @author Notonogoro
  */
-    public class Tugas{
-        private String namaTugas;
-        private Programmer pelaksana;
-        private int status;
-        
-        public Tugas(String nameTugas){
-           this.namaTugas=namaTugas;
-           status=0;
+public class Tugas {
+
+    private String namaTugas;
+    private Programmer pelaksana;
+    private int status;
+
+    public Tugas(String namaTugas) {
+        this.namaTugas = namaTugas;
+        status = 0;
+    }
+
+    public void setPelaksana(Programmer pelaksana, Proyek p) {
+        if(pelaksana.getProgrammerDalamProyek(p) && cekTugasDalamProyek(p)){
+            this.pelaksana = pelaksana;
+            System.out.println("![SUKSES]-Tugas ["+this.namaTugas+"] telah di tambahkan ke programmer "+pelaksana.getNama());
+        } else {
+            System.out.println("![GAGAL]-Programmer "+pelaksana.getNama()+" tidak terdaftar di proyek "+p.getNamaProyek());
         }
-        
-        /*Dilakukan bila Tugas di p == NULL || verifikasiStatus di p == true
-              1. mengubah Tugas di Programmer(langsung saja karena tidak terenkapsulasi) menjadi namaTugas
-              2. mengubah verifikasiStatus di Programmer = false;
-              3. mengubah status di Programmer menjadi = 0;
-              4. mengisi pelaksana = p
-            */
-        
-        public void setPelaksana(Programmer p){
-            if(p.tugas==null || p.verifikasiStatus==true){
-                p.tugas=namaTugas;
-                p.verifikasiStatus=false;
-                p.status=0;
-                pelaksana=p;
+    }
+    
+    public boolean cekTugasDalamProyek(Proyek p){
+        boolean t=false;
+        for(int i =0 ; i<p.getNumTugas();i++){
+            if(p.getTugas(i).equals(this)){
+                t=true;
             }
         }
-        
-        public Programmer getPelaksana(){
-            return pelaksana;
-        }
-        
-        public int getStatus(){
-            this.status=pelaksana.status;
-            if (status == 1) {
-                pelaksana.verifikasiStatus = true;
-            }
-            return status;
-        }
-        
-        public String getNama(){
-            return namaTugas;
-        }
-        
-        public void setNama(){
-            this.namaTugas=namaTugas;
-        }
+        return t;
+    }
+    public String getNamaTugas() {
+        return namaTugas;
+    }
+
+    public void setNamaTugas(String namaTugas) {
+        this.namaTugas = namaTugas;
+    }
+
+    public Programmer getPelaksana() {
+        return pelaksana;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    
 }
